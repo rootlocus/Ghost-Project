4e8c256ec318eccab048d7e14e98244549e52e9f
Eric Heng <erickokkuan@gmail.com>
2021-11-13 15:48:09 +0800

    Add simpler furniture class

---
+++ Assets/Scripts/Entity/Interactables/Furniture.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furniture : Interactable
+{
+    public bool hasClue = false;
+
+    public override void Interact()
+    {
+        PlayDialogue();
+    }
+}
