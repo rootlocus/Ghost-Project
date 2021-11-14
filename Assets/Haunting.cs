71ceba1560b9aea52caa96384607f5575db435f1
Eric Heng <erickokkuan@gmail.com>
2021-11-14 22:01:42 +0800

    add haunting events

--- Assets/Haunting.cs
+++ Assets/Haunting.cs
@@ -6,17 +6,57 @@ public class Haunting : MonoBehaviour
 {
     [Header("Room Sounds")]
     [SerializeField] AudioClip[] ghostSounds;
+    [SerializeField] List<GameObject> horrors;
+    [SerializeField] GameObject demon;
+    BoxCollider2D hauntingZone;
     AudioSource sfxPlayer;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         sfxPlayer = gameObject.GetComponent<AudioSource>();
+        hauntingZone = gameObject.GetComponent<BoxCollider2D>();
+        demon = GameObject.FindGameObjectWithTag("Enemy");
     }
 
     public void PlayRandomSound()
     {
         sfxPlayer.clip = ghostSounds[Random.Range(0, ghostSounds.Length)];
         sfxPlayer.Play();
+    }
+
+    [ContextMenu("Trigger Horror")]
+    public void TriggerHorror()
+    {
+        if (horrors.Count != 0)
+        {
+            horrors[0].SetActive(true);
+            horrors.Remove(horrors[0]);
+        }
+    }
+
+    [ContextMenu("Trigger Scare")]
+    public void TriggerScare()
+    {
+        StartCoroutine("DemonPresense");
+        //get main demon from gamemanager
+        // spawn in room
+        //disappear after a few seconds
+
+    }
+    IEnumerator DemonPresense()
+    {
+        Vector2 hauntHere = RandomPointInBounds(hauntingZone.bounds);
+        demon.transform.position = hauntHere;
+
+        yield return new WaitForSeconds(5);
+        demon.transform.position = new Vector2(1000f, 0f);
+    }
+
+    public static Vector2 RandomPointInBounds(Bounds bounds)
+    {
+        return new Vector2(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y)
+        );
     }
 }
