df1bc1532b130eb214ae7dc2c667ae2e72c70474
Eric Heng <erickokkuan@gmail.com>
2021-12-06 17:35:04 +0800

    improve ui inventory

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -9,24 +9,57 @@ public class InventoryUI : MonoBehaviour
     [SerializeField] GameObject[] slots;
     [SerializeField] Sprite defaultInventorySprite;
     [SerializeField] Sprite selectedInventorySprite;
-    private int selectedIndex = 0;
+    int selectedIndex = 0;
 
-    void Start()
+    void Update() 
     {
-        SelectInventory(selectedIndex);
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            selectedIndex = 0;
+            ToggleItemUse(0);
+        } 
+        else if(Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            selectedIndex = 1;
+            ToggleItemUse(1);
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            selectedIndex = 2;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            selectedIndex = 3;
+        }
+
+        if (selectedIndex != -1)
+        {
+            UseItem(selectedIndex);
+            selectedIndex = -1;
+        }
     }
 
-    private void Update() 
+    public void UseItem(int index)
     {
-        if (Input.GetKeyUp(KeyCode.F))
+        try
         {
-            UseSelectedItem();
-        } else if (Input.GetKeyUp(KeyCode.E))
+            slots[index].GetComponentInChildren<Item>().utilise();
+        }
+        catch (ArgumentOutOfRangeException outOfRange)
         {
-            NextItem();
-        } else if (Input.GetKeyUp(KeyCode.Q))
+            Debug.Log(outOfRange);
+        }
+    }
+
+    public void ToggleItemUse(int index)
+    {
+        if (slots[index].GetComponent<Image>().sprite == selectedInventorySprite)
         {
-            PreviousItem();
+            UnselectInventory(index);
+        }
+        else if (slots[index].GetComponent<Image>().sprite == defaultInventorySprite)
+        {
+            SelectInventory(index);
         }
     }
 
@@ -50,33 +83,33 @@ public class InventoryUI : MonoBehaviour
         }
     }
 
-    public void NextItem()
-    {
-        // Reaching end of inventory
-        if (selectedIndex == slots.Length - 1) {
-            UnselectInventory(selectedIndex);
-            selectedIndex = 0;
-            SelectInventory(selectedIndex);
-        } else {
-            UnselectInventory(selectedIndex);
-            selectedIndex++;
-            SelectInventory(selectedIndex);
-        }
-    }
+    //public void NextItem()
+    //{
+    //    // Reaching end of inventory
+    //    if (selectedIndex == slots.Length - 1) {
+    //        UnselectInventory(selectedIndex);
+    //        selectedIndex = 0;
+    //        SelectInventory(selectedIndex);
+    //    } else {
+    //        UnselectInventory(selectedIndex);
+    //        selectedIndex++;
+    //        SelectInventory(selectedIndex);
+    //    }
+    //}
 
-    public void PreviousItem()
-    {
-        // Reaching start of inventory
-        if (selectedIndex == 0) {
-            UnselectInventory(selectedIndex);
-            selectedIndex = slots.Length - 1;
-            SelectInventory(selectedIndex);
-        } else {
-            UnselectInventory(selectedIndex);
-            selectedIndex--;
-            SelectInventory(selectedIndex);
-        }
-    }
+    //public void PreviousItem()
+    //{
+    //    // Reaching start of inventory
+    //    if (selectedIndex == 0) {
+    //        UnselectInventory(selectedIndex);
+    //        selectedIndex = slots.Length - 1;
+    //        SelectInventory(selectedIndex);
+    //    } else {
+    //        UnselectInventory(selectedIndex);
+    //        selectedIndex--;
+    //        SelectInventory(selectedIndex);
+    //    }
+    //}
 
     public void UseSelectedItem()
     {
